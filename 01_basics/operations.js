4a115a5c3db934b998ce00dbefc6c678fbc7d026
Avantika <82873081+avantikasom@users.noreply.github.com>
2024-08-10 09:55:58 +0000

    operations in js

---
+++ 01_basics/operations.js
@@ -0,0 +1,17 @@
+//operations
+
+console.log(2+2); //4
+console.log(2-2); //0
+console.log(2*2); //4
+console.log(2/2); //1
+console.log(2%2); //0
+console.log(2**3); //8
+
+
+console.log("1" + 2);  //12
+console.log(1 +  "2"); //12
+console.log(1 + 2 + "2");//32
+console.log("2" + 1 + 1);//211
+
+
+console.log(+true); //1
