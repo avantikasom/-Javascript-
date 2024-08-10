246c7f325a10eee572da84e03c40a7ff8be00d8f
Avantika <82873081+avantikasom@users.noreply.github.com>
2024-08-10 08:58:45 +0000

    data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+// we use "use script" in js file to make sure that engine treat the complete js file as a newer version.
+//there are 7 primitive datatype in js
+
+let age = 18 // number -> its capacity is to store 2^53 numbers
+let name = "Avantika" //string
+let isTrue = true //boolean
+let bigint = 34566775  // to store large numbers
+let value = null // a standalone value;
+//undefined 
+//symbol => unique identifier
+
+
+console.log(typeof undefined)
+console.log(typeof null)
+
