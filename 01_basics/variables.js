d175dd03f7283c42c4ee56900c3c5e37442c8b55
Avantika <82873081+avantikasom@users.noreply.github.com>
2024-08-10 08:52:52 +0000

    variables in js

---
+++ 01_basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 234;
+let accountHolderName = "Avantika";
+var accountCity = "Meerut";// It is suggested that not to use var due to problem of scope and functional block.
+accountBranch = "gandhi ashram";// it is also allowed in javascript but not a good practice.
+
+console.table([accountId, accountHolderName, accountCity, accountBranch])// console.table allows us to print the multiple values at once in a table format.
